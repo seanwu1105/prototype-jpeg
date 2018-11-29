f7f571223d1f56144540e5f63a528cc16ba43ccf
Sean Wu <seanwu1105@gmail.com>
2018-11-29 16:44:39 +0800

    remove unncessary pytest import

--- tests/tests_unit/test_codec.py
+++ tests/tests_unit/test_codec.py
@@ -4,7 +4,6 @@ import unittest
 from unittest import mock
 
 import numpy as np
-import pytest
 
 from prototype_jpeg.codec import (
     Encoder, Decoder, decode_huffman, encode_huffman, encode_differential,
